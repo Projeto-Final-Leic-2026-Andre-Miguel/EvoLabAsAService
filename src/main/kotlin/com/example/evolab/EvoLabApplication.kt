9f6cfa9ee736bf2ca30137ae37961e4655720b0b
AndreVaz51585 <andrevaz2005@gmail.com>
2026-03-14 23:48:04 +0000

    Project and dependencies added

---
+++ src/main/kotlin/com/example/evolab/EvoLabApplication.kt
@@ -0,0 +1,11 @@
+package com.example.evolab
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EvoLabApplication
+
+fun main(args: Array<String>) {
+    runApplication<EvoLabApplication>(*args)
+}
