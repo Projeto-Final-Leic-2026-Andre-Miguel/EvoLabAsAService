706918ac0ea1acb93ff3642401525e5bfa2cfa98
AndreVaz51585 <andrevaz2005@gmail.com>
2026-03-15 11:32:26 +0000

    project and dependencies.

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,12 @@
 rootProject.name = "EvoLab"
+
+include(
+    "evolab-frontend",
+    "evolab-backend",
+    "evolab-backend:db",
+    "evolab-backend:domain",
+    "evolab-backend:repo",
+    "evolab-backend:service",
+    "evolab-backend:http",
+    "evolab-backend:app"
+)
